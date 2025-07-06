296d96818589d4a2f27bb5dc36f4909df1f3d209
dan23770Te <lagging.bromide.0l@icloud.com>
2025-07-06 14:37:33 +0100

    Introduce caching mechanism to improve performance in the file

--- zsU4m6/Wy5N0Z/1FA/rollout-strategies_12Dz_1747966142.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='lookahead',
-    version='0.0.2',
-    packages=find_packages(),
-    install_requires=['scikit-learn', 'scipy', 'numpy', 'qmcpy']
-)
